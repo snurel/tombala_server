de17412e9d81bec2f08cd61421ef9726b682f818
Samet Nurel <snurel@gmail.com>
2025-01-01 13:12:43 +0300

    dist folder removed

--- dist/enums/ClientType.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ClientType = void 0;
-var ClientType;
-(function (ClientType) {
-    ClientType["LOBBY"] = "lobby";
-    ClientType["MANAGER"] = "manager";
-    ClientType["PLAYER"] = "player";
-})(ClientType || (exports.ClientType = ClientType = {}));
--- dist/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const UserManager_1 = require("./managers/UserManager");
-const GameManager_1 = require("./managers/GameManager");
-const IOManager_1 = require("./managers/IOManager");
-IOManager_1.IOManager.init();
-GameManager_1.GameManager.init();
-UserManager_1.UserManager.init();
